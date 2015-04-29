924106e1d43ae8b6206cf75b2d7145967087dba9
Jakub Vlasak <vlasakjakub@gmail.com>
2015-04-29 16:05:13 +0200

    Station view controller

---
+++ PragueSubway/StationViewController.h
@@ -0,0 +1,13 @@
+//
+//  StationViewController.h
+//  PragueSubway
+//
+//  Created by Jakub Vlasák on 29/04/15.
+//  Copyright (c) 2015 Jakub Vlasák. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StationViewController : UIViewController
+
+@end
---
+++ PragueSubway/StationViewController.m
@@ -0,0 +1,37 @@
+//
+//  StationViewController.m
+//  PragueSubway
+//
+//  Created by Jakub Vlasák on 29/04/15.
+//  Copyright (c) 2015 Jakub Vlasák. All rights reserved.
+//
+
+#import "StationViewController.h"
+
+@interface StationViewController ()
+
+@end
+
+@implementation StationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
