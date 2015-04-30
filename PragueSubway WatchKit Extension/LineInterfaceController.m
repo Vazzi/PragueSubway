5c13d6a4c77e7fd4cfa557b9a406ee87133a6020
Jakub Vlasak <vlasakjakub@gmail.com>
2015-04-30 18:43:47 +0200

    Station detail data

--- PragueSubway WatchKit Extension/LineInterfaceController.m
+++ PragueSubway WatchKit Extension/LineInterfaceController.m
@@ -11,11 +11,15 @@
 #import "SubwayLine.h"
 #import "Station.h"
 
+#define SEQUE_IDETIFIER_TO_STATION @"toStationDetail"
+
 @interface LineInterfaceController ()
 
 @property (weak, nonatomic) IBOutlet WKInterfaceLabel *titleLabel;
 @property (weak, nonatomic) IBOutlet WKInterfaceTable *stationsTable;
+
 @property (strong, atomic) NSArray *stations;
+@property (strong, atomic) SubwayLine *line;
 
 @end
 
@@ -24,11 +28,18 @@
 - (void)awakeWithContext:(id)context {
     [super awakeWithContext:context];
     
-    SubwayLine *line = context;
-    [self setTitleWithSubwayLine:line];
-    [self setupTableDataWithLine:line];
+    self.line = context;
+    [self setTitleWithSubwayLine:self.line];
+    [self setupTableDataWithLine:self.line];
 }
 
+-(id)contextForSegueWithIdentifier:(NSString *)segueIdentifier inTable:(WKInterfaceTable *)table rowIndex:(NSInteger)rowIndex {
+    if ([segueIdentifier isEqualToString:SEQUE_IDETIFIER_TO_STATION]) {
+        return @{@"station" : self.stations[rowIndex],
+                 @"line" : self.line};
+    }
+    return nil;
+}
 
 - (void)willActivate {
     // This method is called when watch view controller is about to be visible to user
--- PragueSubway WatchKit Extension/StationInterfaceController.m
+++ PragueSubway WatchKit Extension/StationInterfaceController.m
@@ -7,8 +7,21 @@
 //
 
 #import "StationInterfaceController.h"
+#import "Station.h"
+#import "DeparturesTimer.h"
 
 @interface StationInterfaceController ()
+
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *stationNameLabel;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *firstToStationLabel;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *firstTimeLabel;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *secondToStationLabel;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *secondTimeLabel;
+@property (weak, nonatomic) IBOutlet WKInterfaceGroup *secondGroup;
+
+@property (strong, atomic) Station *station;
+@property (strong, atomic) DeparturesTimer *timerFirst;
+@property (strong, atomic) DeparturesTimer *timerSecond;
 
 @end
 
@@ -16,9 +29,27 @@
 
 - (void)awakeWithContext:(id)context {
     [super awakeWithContext:context];
+    NSDictionary *data = context;
     
-    // Configure interface objects here.
+    [self initalizeDataWithStation:(Station *)data[@"station"]
+                              line:(SubwayLine *)data[@"line"]];
+    
+    
 }
+
+- (void)initalizeDataWithStation:(Station *)station line:(SubwayLine *)line {
+    self.station = station;
+    NSArray *departuresGrouped = [station getDeparturesGroupedByDirectionsForLine:line];
+    self.timerFirst = departuresGrouped.firstObject;
+    
+    if (departuresGrouped.count == 2) {
+        self.timerSecond = departuresGrouped.lastObject;
+    } else {
+        self.timerSecond = false;
+    }
+    
+}
+
 
 - (void)willActivate {
     // This method is called when watch view controller is about to be visible to user
