38e1ec1860dda1ce1d96a05bdd3c7ee97e8d725e
Jakub Vlasak <vlasakjakub@gmail.com>
2015-04-30 13:40:49 +0200

    Time string format

--- PragueSubway/DeparturesTimer.m
+++ PragueSubway/DeparturesTimer.m
@@ -80,6 +80,9 @@
     NSInteger seconds = ti % 60;
     NSInteger minutes = (ti / 60) % 60;
     NSInteger hours = (ti / 3600);
+    if (hours == 0) {
+        return [NSString stringWithFormat:@"%02ld:%02ld", (long)minutes, (long)seconds];
+    }
     return [NSString stringWithFormat:@"%01ld:%02ld:%02ld", (long)hours, (long)minutes, (long)seconds];
 }
 
