3a15784ce8d6626337c30c405af48663cd9107a1
Jakub Vlasak <vlasakjakub@gmail.com>
2015-04-30 15:26:34 +0200

    AppleWatchkit setup

---
+++ PragueSubway WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  PragueSubway WatchKit Extension
+//
+//  Created by Jakub Vlasák on 30/04/15.
+//  Copyright (c) 2015 Jakub Vlasák. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
